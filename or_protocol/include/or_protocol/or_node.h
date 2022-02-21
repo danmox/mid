5839f72db1d9ab81f96a045ccc228d7fcac0503d
Daniel Mox <mox@seas.upenn.edu>
2022-02-21 14:10:20 -0500

    ignore loopback messages
    Currently, nodes receive their own broadcasts. While I'm sure there is a better
    way of filtering these messages, currently I'm just dropping all messages
    received with the same sin_addr.s_addr as the node (I did not check the port
    since I found that the varied during testing).

--- or_protocol/include/or_protocol/or_node.h
+++ or_protocol/include/or_protocol/or_node.h
@@ -1,11 +1,10 @@
 #ifndef OR_PROTOCOL_HOST_H_
 #define OR_PROTOCOL_HOST_H_
 
-
 #include <atomic>
+#include <netinet/in.h>
 #include <string>
 #include <thread>
-
 
 namespace or_protocol {
 
@@ -21,6 +20,10 @@ class ORNode
   private:
     int recv_sockfd, port;
     std::string IP;
+
+    // IPv4 address information about this node
+    sockaddr_in my_sa;
+
     std::thread send_thread, recv_thread;
 
     void send_loop();
