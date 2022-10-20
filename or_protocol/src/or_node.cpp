f43776f2d4e087ebbf63772a9dc3b05b5b5923b4
Daniel Mox <mox@seas.upenn.edu>
2022-10-19 22:07:20 -0400

    split test_node into send and recv nodes, add or_protocol_msgs

--- or_protocol/src/or_node.cpp
+++ or_protocol/src/or_node.cpp
@@ -9,6 +9,8 @@ namespace or_protocol {
 
 ORNode::ORNode(std::string _IP, int _port)
 {
+  // TODO use interface name instead? enabling automatic IP address fetching
+
   // initialize BCastSocket and register recv function handle
   using namespace std::placeholders;
   recv_function fcn = std::bind(&ORNode::recv, this, _1, _2);
@@ -18,21 +20,34 @@ ORNode::ORNode(std::string _IP, int _port)
 }
 
 
+void ORNode::send(const char* buff, int size)
+{
+  bcast_socket->send(buff, size);
+}
+
+
 void ORNode::recv(char* buff, int size)
 {
   std::cout << "[ORNode] received " << size << " bytes: " << buff << std::endl;
 }
 
 
-void ORNode::main_loop()
-{
+void ORNode::send_loop() {
   std::string msg = "hello from " + bcast_socket->getIP();
-  const char* msg_str = msg.c_str();
+  const char *msg_str = msg.c_str();
 
   // TODO devise better way of shutting down
   while (run && bcast_socket->run) {
-    bcast_socket->send(msg_str, msg.size());
+    send(msg_str, msg.size());
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  }
+}
+
+
+void ORNode::recv_loop()
+{
+  while (run && bcast_socket->run) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }
 
