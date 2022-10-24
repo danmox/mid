69917816cb52586a457c6ed9dec3d5b70f7743a5
Daniel Mox <mox@seas.upenn.edu>
2022-10-24 16:31:10 -0400

    wrap sending / receiving with or_protocol_msgs/Packet messages

--- or_protocol/src/or_node.cpp
+++ or_protocol/src/or_node.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 
 #include <or_protocol/or_node.h>
+#include <ros/serialization.h>
+
+#include <or_protocol_msgs/Packet.h>
 
 
 namespace or_protocol {
@@ -10,6 +13,9 @@ namespace or_protocol {
 ORNode::ORNode(std::string _IP, int _port)
 {
   // TODO use interface name instead? enabling automatic IP address fetching
+
+  std::string id_str = _IP.substr(_IP.find_last_of('.') + 1);
+  node_id = std::stoi(id_str);
 
   // initialize BCastSocket and register recv function handle
   using namespace std::placeholders;
@@ -20,25 +26,36 @@ ORNode::ORNode(std::string _IP, int _port)
 }
 
 
-void ORNode::send(const char* buff, int size)
+void ORNode::send(const or_protocol_msgs::PacketConstPtr& msg)
 {
-  bcast_socket->send(buff, size);
+  ros::SerializedMessage m = ros::serialization::serializeMessage(*msg);
+  if (!bcast_socket->send(reinterpret_cast<const char*>(m.buf.get()), m.num_bytes)) {
+    std::cout << "[ORNode] failed to send message" << std::endl;
+  }
 }
 
 
-void ORNode::recv(char* buff, int size)
+void ORNode::recv(char* buff, size_t size)
 {
-  std::cout << "[ORNode] received " << size << " bytes: " << buff << std::endl;
+  or_protocol_msgs::Packet msg;
+  // the size of the message is prepended when sent
+  ros::serialization::IStream s(reinterpret_cast<uint8_t*>(buff + 4), size - 4);
+  ros::serialization::deserialize(s, msg);
+
+  printf("%d: %d > %d, seq: %d, %ld bytes\n", node_id, msg.src_id, msg.dest_id,
+         msg.seq, msg.data.size());
 }
 
 
-void ORNode::send_loop() {
-  std::string msg = "hello from " + bcast_socket->getIP();
-  const char *msg_str = msg.c_str();
+void ORNode::send_loop(or_protocol_msgs::PacketPtr& msg) {
+  msg->src_id = node_id;
 
   // TODO devise better way of shutting down
   while (run && bcast_socket->run) {
-    send(msg_str, msg.size());
+    send(msg);
+    printf("%d: %d > %d, seq: %d, %ld bytes\n", node_id, msg->src_id, msg->dest_id,
+           msg->seq, msg->data.size());
+    msg->seq++;
     std::this_thread::sleep_for(std::chrono::milliseconds(5000));
   }
 }
