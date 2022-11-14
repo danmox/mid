8688d81e6354bc1ddfcf7d7da1817b75665df31d
Daniel Mox <mox@seas.upenn.edu>
2022-11-14 15:33:12 -0500

    move send/recv node implementation outside of ORNode
    + implement send/recv loops in their respective nodes
    + automatically fill packet header info in send, unless explicitly told not to

--- or_protocol/src/send_node.cpp
+++ or_protocol/src/send_node.cpp
@@ -7,13 +7,13 @@
 #include <or_protocol_msgs/Packet.h>
 
 
-std::shared_ptr<or_protocol::ORNode> or_node;
+volatile bool run = true;
 
 
 void handler(int s)
 {
   std::cout << "[main] received shutdown signal (" << s << ")" << std::endl;
-  or_node->run = false;
+  run = false;
 }
 
 
@@ -30,15 +30,19 @@ int main(int argc, char** argv)
   siginthandler.sa_flags = 0;
   sigaction(SIGINT, &siginthandler, NULL);
 
-  or_node.reset(new or_protocol::ORNode(argv[1], 4568));
+  or_protocol::ORNode or_node(argv[1], 4568);
 
   // build message
-  or_protocol_msgs::PacketPtr msg(new or_protocol_msgs::Packet);
-  msg->dest_id = 100;
-  msg->seq = 1;
-  msg->data = std::vector<uint8_t>(200, 1);
+  or_protocol_msgs::Packet msg;
+  msg.msg_type = or_protocol_msgs::Packet::PAYLOAD;
+  msg.dest_id = 100;
+  msg.data = std::vector<uint8_t>(200, 1);
 
-  or_node->send_loop(msg);
+  std::cout << "starting send loop" << std::endl;
+  while (run && or_node.run()) {
+    or_node.send(msg);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
 
   return 0;
 }
