8c7fd2a0f16d2c4765621965a8d1c513d165e759
Daniel Mox <mox@seas.upenn.edu>
2022-11-15 17:34:15 -0500

    add ping test (tested between 2 nodes, ~2ms latency)

--- or_protocol/include/or_protocol/or_node.h
+++ or_protocol/include/or_protocol/or_node.h
@@ -19,6 +19,9 @@ namespace or_protocol {
 #define OR_DEBUG(fmt, ...) ROS_DEBUG("[ORNode] " fmt, ##__VA_ARGS__)
 
 
+typedef std::function<void(or_protocol_msgs::Packet&,int,int)> msg_recv_func;
+
+
 class ORNode
 {
   public:
@@ -27,11 +30,13 @@ class ORNode
     bool send(const or_protocol_msgs::PacketConstPtr &msg);
     bool send(const char *buff, size_t size);
     bool run() const {return bcast_socket->run;}
+    void register_recv_func(msg_recv_func fcn);
 
   private:
     std::shared_ptr<BCastSocket> bcast_socket;
     int node_id;
     int seq = 0;
+    msg_recv_func recv_handle = nullptr;
 
     void recv(char* buff, size_t size);
     void print_msg_info(std::string msg, const or_protocol_msgs::Packet& packet);
