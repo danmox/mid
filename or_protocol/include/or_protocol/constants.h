7f09739b71c6acefc73dc0b13b095faabde9923b
Daniel Mox <mox@seas.upenn.edu>
2023-03-10 14:06:00 -0500

    [WIP] add automatic node selection algorithm to NetworkState

--- or_protocol/include/or_protocol/constants.h
+++ or_protocol/include/or_protocol/constants.h
@@ -6,7 +6,7 @@ namespace or_protocol {
 
 
 // port used by the OR protocol
-const int OR_PROTOCOL_PORT = 4568;
+#define OR_PROTOCOL_PORT 4568
 
 // minimum unit of delay for enforcing relay priority in nanoseconds
 #define UNIT_DELAY 10000000
@@ -24,6 +24,22 @@ const int OR_PROTOCOL_PORT = 4568;
 // messages reach their intended destinations and aren't being relayed any
 // longer and no longer need to be tracked
 #define MSG_BUFFER_CAPACITY 200
+
+// thresholds for determining if a candidate relay node is "close" to the
+// current node in ETX
+#define CLOSENESS_FACTOR 2.0
+#define CLOSENESS_THRESHOLD 4.0
+
+// threshold for determining of a candidate relay is close enough to the default
+// path
+#define NEAR_PATH_THRESHOLD 2.0
+
+// the maximum number of relays to use at each hop
+#define MAX_RELAY_COUNT 4
+
+// the maximum allowable ETX between relays of the same transmitter
+#define RELAY_ETX_THRESHOLD 2.0
+
 
 }  // namespace or_protocol
 
