b3392c168ad927292c13bf5270267c583bb83192
Daniel Mox <mox@seas.upenn.edu>
2023-04-17 17:19:58 -0400

    clean up test_node and or_node

--- or_protocol/src/test_node.cpp
+++ or_protocol/src/test_node.cpp
@@ -6,21 +6,21 @@
 #include <mutex>
 #include <yaml-cpp/yaml.h>
 
+#include <rosbag/bag.h>
 #include <or_protocol/or_protocol.h>
 #include <or_protocol/utils.h>
+
 #include <or_protocol_msgs/Packet.h>
-#include <ros/console.h>
-#include <rosbag/bag.h>
 #include <std_msgs/UInt32.h>
+
+
+namespace fs = std::filesystem;
+using ros::serialization::serializationLength;
 
 
 volatile bool run = true;
 rosbag::Bag bag;
 std::mutex bag_mutex;
-
-
-namespace fs = std::filesystem;
-using ros::serialization::serializationLength;
 
 
 struct FlowSpec
@@ -33,7 +33,7 @@ struct FlowSpec
 
 void signal_handler(int s)
 {
-  ROS_INFO("[main] received shutdown signal %d", s);
+  ROS_DEBUG("[main] received shutdown signal %d", s);
   run = false;
 }
 
@@ -46,7 +46,7 @@ void msg_cb(ros::Time recv_time, or_protocol_msgs::Packet& pkt, int node_id, int
   or_protocol::deserialize(seq, pkt.data.data(), pkt.data.size());
 
   std::lock_guard<std::mutex> lock(bag_mutex);
-  bag.write("recv_seq_numbers", recv_time, seq);
+  bag.write("/recv_seq_numbers", recv_time, seq);
 }
 
 
@@ -57,6 +57,9 @@ int main(int argc, char** argv)
     return 0;
   }
 
+  std::string ip_str(argv[1]);
+  int node_id = std::stoi(ip_str.substr(ip_str.find_last_of('.') + 1));
+
   // load flows and etx table from yaml
   fs::path sample_path(argv[2]);
   if (!fs::is_regular_file(sample_path)) {
@@ -64,30 +67,24 @@ int main(int argc, char** argv)
     return 0;
   }
   YAML::Node sample = YAML::LoadFile(sample_path.string());
-  or_protocol::ETXMap sample_link_etx;
-  if (!sample["link_etx"] || !sample["flows"]) {
-    std::cout << "[main] 'link_etx' and 'flows' must be specified" << std::endl;
+  if (!sample["flows"]) {
+    std::cout << "[main] 'flows' must be specified" << std::endl;
     return 0;
-  }
-  for (const auto& node : sample["link_etx"]) {
-    const int& tx_node = node.first.as<int>();
-    const YAML::Node& map_node = node.second;
-
-    std::unordered_map<int, double> inner_map;
-    for (auto& inner_node : map_node) {
-      const int& rx_node = inner_node.first.as<int>();
-      const double& etx = inner_node.second.as<double>();
-      inner_map.emplace(rx_node, etx);
-    }
-
-    sample_link_etx.emplace(tx_node, inner_map);
   }
   std::unordered_map<int, std::unordered_map<int, FlowSpec>> flows;
   for (const auto& node : sample["flows"]) {
+    if (!node["src"] || !node["dest"] || !node["reliable"] || !node["size"]) {
+      std::cout << "[main] each flow must contain 'src', 'dest', 'reliable', and";
+      std::cout << " 'size' fields" << std::endl;
+      return 0;
+    }
     int src = node["src"].as<int>();
     int dest = node["dest"].as<int>();
     bool reliable = node["reliable"].as<bool>();
     int size = node["size"].as<int>();
+
+    if (src != node_id)
+      continue;
 
     or_protocol_msgs::Packet pkt;
     pkt.header.msg_type = or_protocol_msgs::Header::PAYLOAD;
@@ -100,7 +97,22 @@ int main(int argc, char** argv)
 
     flows[src][dest] = {pkt, seq, size};
   }
-  size_t total_msgs = sample["total_msgs"].as<size_t>();
+  int total_msgs = 0;
+  if (flows.size() > 0 && sample["total_msgs"])
+    total_msgs = sample["total_msgs"].as<int>();
+
+  if (flows.size() > 0) {
+    for (auto& src : flows) {
+      for (auto& dest : src.second) {
+        const FlowSpec& flow = dest.second;
+        ROS_INFO("flow: %d > %d, size=%d, reliable=%s",
+                 flow.packet.header.src_id, flow.packet.header.dest_id,
+                 flow.total_size, flow.packet.header.reliable ? "true" : "false");
+      }
+    }
+  } else {
+    ROS_INFO("no flows originating at this node");
+  }
 
   struct sigaction siginthandler;
   siginthandler.sa_handler = signal_handler;
@@ -108,22 +120,14 @@ int main(int argc, char** argv)
   siginthandler.sa_flags = 0;
   sigaction(SIGINT, &siginthandler, NULL);
 
-  std::string log_file = "or_node.bag";
-  rosbag::Bag bag;
-  bag.open(log_file, rosbag::bagmode::Write);
-  if (!bag.isOpen()) {
-    ROS_FATAL("[main] failed to open log file: %s", log_file.c_str());
-    exit(EXIT_FAILURE);
-  } else {
-    ROS_INFO("[main] logging received messages to %s", log_file.c_str());
-  }
-
   or_protocol::ORProtocol or_node(argv[1]);
-  or_protocol::ORProtocolTest or_node_test;
-  or_node_test.initializeORNode(or_node, sample_link_etx);
   or_node.register_recv_func(msg_cb);
 
-  ROS_INFO("[main] sending %ld messages", total_msgs);
+  bag.open("test_node.bag", rosbag::bagmode::Write);
+
+  if (total_msgs != 0)
+    ROS_INFO("[main] sending %d messages", total_msgs);
+  int sent_msgs = 0;
   while (run && or_node.is_running()) {
     for (auto& src : flows) {
       for (auto& dest : src.second) {
@@ -139,12 +143,18 @@ int main(int argc, char** argv)
         or_node.send(flow.packet);
         {
           std::lock_guard<std::mutex> lock(bag_mutex);
-          bag.write("send_seq_numbers", ros::Time::now(), flow.seq);
+          bag.write("/send_seq_numbers", ros::Time::now(), flow.seq);
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
         flow.seq.data++;
       }
     }
+
+    if (total_msgs != 0 && !(sent_msgs < total_msgs))
+      break;
+
+    ++sent_msgs;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   ROS_INFO("[main] sleeping for 4 seconds to allow all messages to transmit");
