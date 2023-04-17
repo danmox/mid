ba00c1959119a509c59400621e5ff162de5f2edf
Daniel Mox <mox@seas.upenn.edu>
2023-04-17 17:04:59 -0400

    change packet log topic

--- or_protocol/src/or_protocol.cpp
+++ or_protocol/src/or_protocol.cpp
@@ -196,7 +196,7 @@ void ORProtocol::log_message(const or_protocol_msgs::Header& header,
                              const int size,
                              const ros::Time& time)
 {
-  static const std::string topic = "node" + std::to_string(node_id);
+  static const std::string topic = "/node" + std::to_string(node_id) + "/log";
   Log msg;
   msg.header = header;
   msg.action = action;
