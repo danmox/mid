86ec8ee50ba3a81f19ffae1ab01fcd1427b6c5ed
Daniel Mox <mox@seas.upenn.edu>
2023-07-10 11:46:09 -0400

    apply .clang-format to or_node.h and improve pair hashing

--- or_protocol/include/or_protocol/or_node.h
+++ or_protocol/include/or_protocol/or_node.h
@@ -1,9 +1,9 @@
 #ifndef OR_PROTOCOL_OR_NODE_H_
 #define OR_PROTOCOL_OR_NODE_H_
 
-#include <vector>
-#include <utility>
 #include <unordered_map>
+#include <utility>
+#include <vector>
 
 #include <or_protocol/or_protocol.h>
 #include <ros/ros.h>
@@ -24,44 +24,44 @@ namespace or_protocol {
 
 struct PairHash
 {
-    template <typename T1, typename T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const
-    {
-      auto h1 = std::hash<T1>{}(p.first);
-      auto h2 = std::hash<T2>{}(p.second);
-      return h1 ^ h2;
-    }
+  template <typename T1, typename T2>
+  std::size_t operator()(const std::pair<T1, T2>& p) const
+  {
+    auto h1 = std::hash<T1>{}(p.first);
+    auto h2 = std::hash<T2>{}(p.second);
+    return h1 ^ (h2 << 1);
+  }
 };
 
 struct PairEqual
 {
-    template <typename T1, typename T2>
-    bool operator()(const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const
-    {
-      return p1.first == p2.first && p1.second == p2.second;
-    }
+  template <typename T1, typename T2>
+  bool operator()(const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const
+  {
+    return p1.first == p2.first && p1.second == p2.second;
+  }
 };
 
 
 struct SubInfo
 {
-    std::string topic;
-    int queue_size;
-    bool reliable;
-    bool synced;
-    unsigned int dest_id;
-    std::unique_ptr<ros::Subscriber> sub;
+  std::string topic;
+  int queue_size;
+  bool reliable;
+  bool synced;
+  unsigned int dest_id;
+  std::unique_ptr<ros::Subscriber> sub;
 };
 
 
 struct PubInfo
 {
-    std::string name;
-    std::string md5sum;
-    std::string type;
-    std::string definition;
-    std::string latching;
-    std::unique_ptr<ros::Publisher> pub;
+  std::string name;
+  std::string md5sum;
+  std::string type;
+  std::string definition;
+  std::string latching;
+  std::unique_ptr<ros::Publisher> pub;
 };
 
 
@@ -73,15 +73,15 @@ bool get_param(const XmlRpc::XmlRpcValue& val,
 {
   static std::unordered_map<XmlRpc::XmlRpcValue::Type, std::string>
     type_strings = {
-    {XmlRpc::XmlRpcValue::TypeInvalid, "TypeInvalid"},
-    {XmlRpc::XmlRpcValue::TypeBoolean, "TypeBoolean"},
-    {XmlRpc::XmlRpcValue::TypeInt, "TypeInt"},
-    {XmlRpc::XmlRpcValue::TypeDouble, "TypeDouble"},
-    {XmlRpc::XmlRpcValue::TypeString, "TypeString"},
-    {XmlRpc::XmlRpcValue::TypeDateTime, "TypeDateTime"},
-    {XmlRpc::XmlRpcValue::TypeBase64, "TypeBase64"},
-    {XmlRpc::XmlRpcValue::TypeArray, "TypeArray"},
-    {XmlRpc::XmlRpcValue::TypeStruct, "TypeStruct"}};
+      {XmlRpc::XmlRpcValue::TypeInvalid, "TypeInvalid"},
+      {XmlRpc::XmlRpcValue::TypeBoolean, "TypeBoolean"},
+      {XmlRpc::XmlRpcValue::TypeInt, "TypeInt"},
+      {XmlRpc::XmlRpcValue::TypeDouble, "TypeDouble"},
+      {XmlRpc::XmlRpcValue::TypeString, "TypeString"},
+      {XmlRpc::XmlRpcValue::TypeDateTime, "TypeDateTime"},
+      {XmlRpc::XmlRpcValue::TypeBase64, "TypeBase64"},
+      {XmlRpc::XmlRpcValue::TypeArray, "TypeArray"},
+      {XmlRpc::XmlRpcValue::TypeStruct, "TypeStruct"}};
 
   std::string val_str = val.toXml();
   const char* vcstr = val_str.c_str();
@@ -105,30 +105,30 @@ bool get_param(const XmlRpc::XmlRpcValue& val,
 
 class ORNode
 {
-  public:
-    ORNode(const ros::NodeHandle& _nh, const ros::NodeHandle& _pnh);
+ public:
+  ORNode(const ros::NodeHandle& _nh, const ros::NodeHandle& _pnh);
 
-    void recv_packet(ros::Time& recv_time, or_protocol_msgs::Packet& pkt, int size);
+  void recv_packet(ros::Time& recv_time, or_protocol_msgs::Packet& pkt, int size);
 
-    void ros_msg_cb(const topic_tools::ShapeShifter::ConstPtr& msg, int idx);
+  void ros_msg_cb(const topic_tools::ShapeShifter::ConstPtr& msg, int idx);
 
-    bool running() { return run; }
+  bool running() { return run; }
 
-  private :
-    volatile bool run;
+ private:
+  volatile bool run;
 
-    ros::NodeHandle nh, pnh;
-    ros::Publisher status_pub, table_pub;
+  ros::NodeHandle nh, pnh;
+  ros::Publisher status_pub, table_pub;
 
-    std::shared_ptr<ORProtocol> protocol;
+  std::shared_ptr<ORProtocol> protocol;
 
-    std::vector<SubInfo> subscribers;
-    typedef std::pair<int, int> PubKey;
-    std::unordered_map<PubKey, PubInfo, PairHash, PairEqual> publishers;
+  std::vector<SubInfo> subscribers;
+  typedef std::pair<int, int> PubKey;
+  std::unordered_map<PubKey, PubInfo, PairHash, PairEqual> publishers;
 };
 
 
-} // namespace or_protocol
+}  // namespace or_protocol
 
 
 #endif
