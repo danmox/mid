54867a2395e71a095b7e0fc63bc3eff0cc11c7c7
Daniel Mox <mox@seas.upenn.edu>
2023-07-16 16:01:22 -0400

    add xtion camera check to startup script

--- perch_experiment/scripts/start.sh
+++ perch_experiment/scripts/start.sh
@@ -6,21 +6,30 @@ if [[ "$#" -eq 1 ]]; then
 fi
 
 if tmux has-session -t ros &>/dev/null; then
-  echo "tmux sesesion 'ros' already running!"
+  echo "tmux session 'ros' already running!"
   exit
 fi
 
 MID_ID=${AGENT:(-2)}
 ROS_IP=192.168.0.$MID_ID
+ROS_MASTER_URI=http://$ROS_IP:11311
 
 echo "starting robot $MID_ID with type: $TYPE"
 tmux new-session -d -s ros -n mid "source ~/mid_ws/devel/setup.bash; export ROS_IP=$ROS_IP; export ROS_MASTER_URI=http://$ROS_IP:11311; roslaunch perch_experiment scarab.launch type:=$TYPE"
 
-echo "waiting for 10s to check if startup was successful"
-sleep 10.0
+echo "checking if xtion started successfully..."
+sleep 5.0
 
-if tmux has-session -t ros &>/dev/null; then
-  echo "successfully started robot"
+source ~/mid_ws/devel/setup.bash
+export ROS_IP=$ROS_IP
+export ROS_MASTER_URI=$ROS_MASTER_URI
+timeout 5 rostopic echo -n1 --noarr /$AGENT/scan_merged/header/seq
+
+RES=$?
+if [[ "$RES" -eq 124 ]]; then
+  echo "timeout reached waiting for xtion"
+elif [[ "$RES" -eq 0 ]]; then
+  echo "everything looks good!"
 else
-  echo "robot not running!"
+  echo "RES=$RES; robot may not have started successfully"
 fi
