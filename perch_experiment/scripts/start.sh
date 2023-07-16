6391437046893923e559c6e8ab5d2317e1888226
Daniel Mox <mox@seas.upenn.edu>
2023-07-15 23:23:54 -0400

    print more information in start.sh

--- perch_experiment/scripts/start.sh
+++ perch_experiment/scripts/start.sh
@@ -11,14 +11,15 @@ if tmux has-session -t ros &>/dev/null; then
 fi
 
 MID_ID=${AGENT:(-2)}
+ROS_IP=192.168.0.$MID_ID
 
-echo "starting robot with type: $TYPE"
-tmux new-session -d -s ros -n mid "source ~/mid_ws/devel/setup.bash; export ROS_IP=192.168.0.$MID_ID; export ROS_MASTER_URI=http://$ROS_IP:11311; roslaunch perch_experiment scarab.launch type:=$TYPE"
+echo "starting robot $MID_ID with type: $TYPE"
+tmux new-session -d -s ros -n mid "source ~/mid_ws/devel/setup.bash; export ROS_IP=$ROS_IP; export ROS_MASTER_URI=http://$ROS_IP:11311; roslaunch perch_experiment scarab.launch type:=$TYPE"
 
 echo "waiting for 10s to check if startup was successful"
 sleep 10.0
 
-if tmux has-session -t ros; then
+if tmux has-session -t ros &>/dev/null; then
   echo "successfully started robot"
 else
   echo "robot not running!"
