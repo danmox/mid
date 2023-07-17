b83768fb58f613cfc7a2df36859d186bd88cb8e1
Daniel Mox <mox@seas.upenn.edu>
2023-07-17 04:30:35 -0400

    wait for shutdown in stop.sh

--- perch_experiment/scripts/stop.sh
+++ perch_experiment/scripts/stop.sh
@@ -5,4 +5,11 @@ if tmux has-session -t ros &>/dev/null; then
   tmux send-keys -t ros:mid.0 C-c Enter
 else
   echo "no tmux session 'ros' found"
+  exit
 fi
+
+echo "waiting for shutdown to complete..."
+while tmux has-session -t ros &>/dev/null; do
+  sleep 1.0
+done
+echo "shutdown complete"
