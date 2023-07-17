e6096e11a6a066b0ba9208ee8b23bebcf6340ce1
Daniel Mox <mox@seas.upenn.edu>
2023-07-17 04:30:49 -0400

    check if mid-adhoc... is running before launching nodes in start.sh

--- perch_experiment/scripts/start.sh
+++ perch_experiment/scripts/start.sh
@@ -15,6 +15,13 @@ if [[ -z "$AGENT" ]]; then
   exit
 fi
 
+if systemctl is-active --quiet mid-adhoc@wifi2.service; then
+  echo "mid-adhoc@wifi2.service active"
+else
+  echo "mid-adhoc@wifi2.service not active!"
+  exit
+fi
+
 MID_ID=${AGENT:(-2)}
 ROS_IP=192.168.0.$MID_ID
 ROS_MASTER_URI=http://$ROS_IP:11311
@@ -22,21 +29,23 @@ ROS_MASTER_URI=http://$ROS_IP:11311
 echo "starting robot $MID_ID with type: $TYPE"
 tmux new-session -d -s ros -n mid "source ~/mid_ws/devel/setup.bash; export ROS_IP=$ROS_IP; export ROS_MASTER_URI=http://$ROS_IP:11311; roslaunch perch_experiment scarab.launch type:=$TYPE"
 
-echo "sleeping for 5.0 seconds to let nodes start up"
-sleep 5.0
-
-echo "checking if xtion started successfully..."
 source ~/mid_ws/devel/setup.bash
 export ROS_IP=$ROS_IP
 export ROS_MASTER_URI=$ROS_MASTER_URI
 
+echo "checking if xtion started successfully"
+MSG="sleeping for 5.0 seconds to let nodes start up..."
 for i in {1..2}; do
+  echo "$MSG"
+  sleep 5.0
+
   echo "attempt $i"
   timeout 5 rostopic echo -n1 --noarr /$AGENT/scan_merged/header/seq
 
   RES=$?
   if [[ "$RES" -eq 124 ]]; then
     echo "timeout reached waiting for xtion"
+    MSG="sleeping for 5.0 seconds before trying again..."
   elif [[ "$RES" -eq 0 ]]; then
     echo "everything looks good!"
     exit
@@ -44,7 +53,4 @@ for i in {1..2}; do
     echo "RES=$RES; robot may not have started successfully"
     exit
   fi
-
-  echo "sleeping for 5.0 seconds before trying again..."
-  sleep 5.0
 done
