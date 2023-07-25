0edb3c557a48801dd093645378cb963521b8ac3a
Daniel Mox <mox@seas.upenn.edu>
2023-07-24 23:15:01 -0400

    fix stop.sh since pane no longer exits with the process

--- perch_experiment/scripts/stop.sh
+++ perch_experiment/scripts/stop.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if tmux has-session -t ros &>/dev/null; then
+  echo "sending C-c to tmux pane"
+  tmux send-keys -t ros:mid.0 C-c Enter
   echo "capturing screen output"
   tmux capture-pane -t ros:mid.0 -pS - > ~/tmux-ros-mid.txt
-  echo "shutting down tmux session 'ros'"
-  tmux send-keys -t ros:mid.0 C-c Enter
 else
   echo "no tmux session 'ros' found"
   exit
@@ -12,6 +12,11 @@ fi
 
 echo "waiting for shutdown to complete..."
 while tmux has-session -t ros &>/dev/null; do
+  PANE_DEAD=$(tmux list-panes -t ros:mid.0 -F "#{pane_dead}")
+  if [[ "$PANE_DEAD" == "1" ]]; then
+    echo "tmux pane dead, killing session"
+    tmux kill-session -t ros
+  fi
   sleep 1.0
 done
 echo "shutdown complete"
